feb8e165a0628bdfa1c85955a96f2bee2cdaf4d8
Saurav Dhyani <postsaurav@users.noreply.github.com>
2024-05-08 01:01:22 +0530

    Add files via upload

---
+++ Setup Environment/Setup - SSL NavUserPassword in #MSDyn365bc BC24 WebServices & API.ps1
@@ -0,0 +1,15 @@
+﻿# How to Setup SSL Certificate in Business Central 2024 Release Wave 1 WebServices and API
+# Credential Types - NavUserPassword
+# Author - Saurav Dhyani
+# Date - 08-May-2024
+
+# Import Modules
+Import-Module 'C:\Program Files\Microsoft Dynamics 365 Business Central\240\Service\NavAdminTool.ps1'
+
+# Update Service Parameters
+$ServiceName = 'BC240'
+Set-NAVServerConfiguration -ServerInstance $ServiceName -KeyName SOAPServicesEnabled -KeyValue true
+Set-NAVServerConfiguration -ServerInstance $ServiceName -KeyName ODataServicesEnabled -KeyValue true
+Set-NAVServerConfiguration -ServerInstance $ServiceName -KeyName SOAPServicesSSLEnabled -KeyValue true
+Set-NAVServerConfiguration -ServerInstance $ServiceName -KeyName ODataServicesSSLEnabled -KeyValue true
+Restart-NAVServerInstance -ServerInstance $ServiceName
