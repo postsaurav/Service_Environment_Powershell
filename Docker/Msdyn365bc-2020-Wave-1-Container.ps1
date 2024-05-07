1f8e82372637e3c87f9369556963b90c1d8719e0
Saurav Dhyani <sdhyani@archerpoint.com>
2024-05-07 23:28:09 +0530

    Old Branches Merged.

---
+++ Docker/Msdyn365bc-2020-Wave-1-Container.ps1
@@ -0,0 +1,27 @@
+﻿#Author - Saurav Dhyani
+#Refrence Blog - MSDYN365BC 2020 Wave 1 - Set up Docker Container Preview?
+
+#Set Execution Policy
+Set-ExecutionPolicy RemoteSigned -Force
+
+# Install Nav Container Helper Module
+Install-Module -Name navcontainerhelper
+
+#Setup Docker Container.
+#Change Values for $imageName, $ContainerName, username, Password, $LicenseFile & Memory Limit as per your requirment.
+
+$imageName = "bcinsider.azurecr.io/bcsandbox-master:us"
+$containerName = "BC3652020Wave1"
+$auth = "UserPassword"
+$credential = New-Object pscredential 'admin', (ConvertTo-SecureString -String 'P@ssword1' -AsPlainText -Force)
+$licenseFile = "D:\BC15Lic.flf"
+
+New-BCContainer -accept_eula `
+                -imageName $imageName `
+                -containerName $containerName `
+                -auth $auth `
+                -credential $credential `
+                -licenseFile $licenseFile `
+                -updateHosts `
+                -includeAL `
+                -memoryLimit 3G
---
+++ Setup Environment/Setup - NavUserPassword in #MSDyn365bc.ps1
@@ -0,0 +1,21 @@
+﻿# How to Setup Credentail Type NavUserPassword in Business Central 2024 Release Wave 1
+# Credential Types - NavUserPassword
+# Author - Saurav Dhyani
+# Date - 07-May-2024
+
+# Import Modules
+Import-Module 'C:\Program Files\Microsoft Dynamics 365 Business Central\240\Service\NavAdminTool.ps1'
+
+#Add User To Business central Environment
+$ServiceName = 'BC240'
+New-NAVServerUser -ServerInstance $ServiceName -UserName saurav -FullName "Saurav Dhyani" -Password (Read-Host "Enter Password" -AsSecureString)
+New-NAVServerUserPermissionSet -PermissionSetId SUPER -ServerInstance $ServiceName -UserName saurav
+
+#Changing Client Service Credential Type
+$ServiceName = 'BC240'
+Set-NAVServerConfiguration -ServerInstance $ServiceName -KeyName ClientServicesCredentialType -KeyValue NavUserPassword
+Restart-NAVServerInstance -ServerInstance $ServiceName
+
+
+#Change Client Web Service Credential Type
+Set-NAVWebServerInstanceConfiguration -WebServerInstance $ServiceName -KeyName ClientServicesCredentialType -KeyValue NavUserPassword
